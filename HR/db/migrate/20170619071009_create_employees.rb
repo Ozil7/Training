33528d45b11c0f026d7489659a2a1cff37b6821d
user <rohan.k@reportgarden.com>
2017-06-20 10:09:56 +0530

    Rails Assignment

---
+++ HR/db/migrate/20170619071009_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :employees do |t|
+    	t.string :name
+      t.integer :age
+      t.string :designation
+      t.string :project
+      t.string :lead
+
+
+      t.timestamps
+    end
+  end
+end
